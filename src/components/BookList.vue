bb8b2d3b1e2cd2c41baca47ef3cf5d9d828eab66
Liuba Kuibida <k2.liuba@gmail.com>
2022-10-06 14:37:46 +0300

    add loader on sorting

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,15 +2,27 @@
 import { ref } from 'vue'
 import { useBooksStore } from '../stores/books'
 import BookColumn from './BookColumn.vue'
+import LoaderIcon from './LoaderIcon.vue'
 
 const bookStore = useBooksStore()
 const sortByYear = ref(false)
+const showLoader = ref(false)
+
+const sortWithDelay = () => {
+  showLoader.value = true;
+  setTimeout(() => {
+    sortByYear.value = !sortByYear.value;
+    showLoader.value = false;
+  }, 500);
+};
 </script>
 
 <template>
   <div class="bookshelf">
     <div class="row u-center">
-      <button class="btn-link" @click="sortByYear = !sortByYear">Sort by year</button>
+      <button class="btn-link sort-button" @click="sortWithDelay()">Sort by year
+        <LoaderIcon v-if="showLoader" />
+      </button>
     </div>
     <div v-if="sortByYear" class="row">
       <BookColumn v-for="book in bookStore.booksByYear" :key="book.id" :book="book" />
@@ -42,5 +54,28 @@ const sortByYear = ref(false)
       animations can be calculated correctly. */
 .fade-leave-active {
   position: absolute;
+}
+
+@keyframes rotating {
+    from{
+        -webkit-transform: rotate(0deg);
+    }
+    to{
+        -webkit-transform: rotate(360deg);
+    }
+}
+
+.sort-button {
+  position: relative;
+  min-width: 12rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.sort-button svg {
+  position: absolute;
+  right: 20px;
+  animation: rotating 2s linear infinite;
 }
 </style>
